54f434e80d079c011f0dde730aacc0f9dae0d263
Scott Serage <xttocsurge51@gmail.com>
2018-08-21 11:07:50 -0600

    Completed queries step

--- src/components.js
+++ src/components.js
@@ -1,11 +1,15 @@
+import dogphoto from './components/DogPhoto';
 import dogs from './components/Dogs';
 import exchangerates from './components/ExchangeRates';
+
+export const DogPhoto = dogphoto;
 
 export const Dogs = dogs;
 
 export const ExchangeRates = exchangerates;
 
 export default {
+    DogPhoto,
     Dogs,
     ExchangeRates,
 }
--- src/index.js
+++ src/index.js
@@ -3,21 +3,44 @@ import { render } from "react-dom";
 import { ApolloProvider } from "react-apollo";
 import { unregister } from './registerServiceWorker';
 import ApolloClient from "apollo-boost";
-import Dogs from './components/Dogs';
+import { DogPhoto, Dogs } from './components';
 
 const client = new ApolloClient({
 //   uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
     uri: 'https://nx9zvp49q7.lp.gql.zone/graphql'
 });
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <div>
-      <h2>My 2nd Apollo app 🚀</h2>
-      <Dogs />
-    </div>
-  </ApolloProvider>
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDog: null,
+        }
+    }
+
+    onDogSelected = ({ target }) => {
+        this.setState({
+            selectedDog: target.value,
+        })
+    }
+
+    render() {
+        return (
+            <ApolloProvider client={client}>
+                <div>
+                    <h2>My 2nd Apollo app 🚀</h2>
+                    {
+                        this.state.selectedDog &&
+                        <DogPhoto breed={this.state.selectedDog}/>
+                    }
+                    <Dogs onDogSelected={this.onDogSelected}/>
+                </div>
+            </ApolloProvider>
+        )
+    }
+}
+  
+
 
 render(<App />, document.getElementById("root"));
 unregister();
