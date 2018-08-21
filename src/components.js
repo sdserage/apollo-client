46b8c040843549ccb4244d884a64a2327d02ef8f
Scott Serage <xttocsurge51@gmail.com>
2018-08-21 10:41:14 -0600

    Updated app for 2nd step, changed client uri

---
+++ src/components.js
@@ -0,0 +1,11 @@
+import dogs from './components/Dogs';
+import exchangerates from './components/ExchangeRates';
+
+export const Dogs = dogs;
+
+export const ExchangeRates = exchangerates;
+
+export default {
+    Dogs,
+    ExchangeRates,
+}
--- src/index.js
+++ src/index.js
@@ -3,42 +3,18 @@ import { render } from "react-dom";
 import { ApolloProvider } from "react-apollo";
 import { unregister } from './registerServiceWorker';
 import ApolloClient from "apollo-boost";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
-
-const ExchangeRates = () => (
-  <Query
-    query={gql`
-      {
-        rates(currency: "USD") {
-          currency
-          rate
-        }
-      }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :(</p>;
-
-      return data.rates.map(({ currency, rate }) => (
-        <div key={currency}>
-          <p>{`${currency}: ${rate}`}</p>
-        </div>
-      ));
-    }}
-  </Query>
-);
+import Dogs from './components/Dogs';
 
 const client = new ApolloClient({
-  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+//   uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+    uri: 'https://nx9zvp49q7.lp.gql.zone/graphql'
 });
 
 const App = () => (
   <ApolloProvider client={client}>
     <div>
       <h2>My 2nd Apollo app 🚀</h2>
-      <ExchangeRates />
+      <Dogs />
     </div>
   </ApolloProvider>
 );
