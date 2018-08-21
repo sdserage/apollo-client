5f3cc4ee80fbc93477bd71610377d5bbb5620435
Scott Serage <xttocsurge51@gmail.com>
2018-08-21 09:43:20 -0600

    Added query from example

--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import { unregister } from './registerServiceWorker';
 import ApolloClient from "apollo-boost";
+import { Query } from "react-apollo";
 import gql from "graphql-tag";
+
+const ExchangeRates = () => (
+  <Query
+    query={gql`
+      {
+        rates(currency: "USD") {
+          currency
+          rate
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return data.rates.map(({ currency, rate }) => (
+        <div key={currency}>
+          <p>{`${currency}: ${rate}`}</p>
+        </div>
+      ));
+    }}
+  </Query>
+);
 
 const client = new ApolloClient({
   uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
 });
 
-client
-  .query({
-    query: gql`
-      {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My 2nd Apollo app 🚀</h2>
+      <ExchangeRates />
+    </div>
+  </ApolloProvider>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<App />, document.getElementById("root"));
+unregister();
